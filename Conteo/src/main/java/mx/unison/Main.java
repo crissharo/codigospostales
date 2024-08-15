0050a2bf1858e7b25104c2fc01678505a9ccc7eb
crissharo <a223202513@unison.mx>
2024-08-14 20:12:33 -0700

    Update Main.java

--- Conteo/src/main/java/mx/unison/Main.java
+++ Conteo/src/main/java/mx/unison/Main.java
@@ -6,8 +6,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        // Nueva ruta del archivo
-        String archivo = "codigos_postales.csv"; // Ajusta según la ubicación de tu archivo Java
+
+        String archivo = "codigos_postales.csv";
 
         int conteoUrbanos = 0;
         int conteoRurales = 0;
@@ -15,8 +15,6 @@ public class Main {
         try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
             String linea;
 
-            // Si el archivo tiene una cabecera, puedes leerla y descartarla aquí
-            // br.readLine();
 
             while ((linea = br.readLine()) != null) {
                 String[] columnas = linea.split(",");
